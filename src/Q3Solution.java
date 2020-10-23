19e4bbb367d528c4e0daebd34a1dff0ec0e1d8ae
wzy27 <wzy27@users.noreply.github.com>
2020-10-23 21:19:36 +0800

    Refined Q4Solution and combined Q3 and Q4

--- src/Q3Solution.java
+++ src/Q3Solution.java
@@ -1,95 +1,14 @@
-import java.util.Arrays;
-import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Q3Solution {
-
-    private static class searchContext {
-
-        int currentNode;
-        int hospital;
-
-        searchContext(int c, int h) {
-            currentNode = c;
-            hospital = h;
-        }
-    }
-
     /**
      * Solution for question 3 in lab 2.
      *
      * @param graph A undirected unweighted graph representing a city road network.
      * @return
      */
-    public static void solve(CityGraph graph) {
-
-        int[] pre1 = new int[graph.V()],
-                pre2 = new int[graph.V()],
-                hos1 = new int[graph.V()],
-                hos2 = new int[graph.V()];
-        Arrays.fill(pre1, -2);
-        Arrays.fill(pre2, -2);
-        Arrays.fill(hos1, -2);
-        Arrays.fill(hos2, -2);
-
-        LinkedList<searchContext> queue = new LinkedList<>();
-        for (int h : graph.allH()) {
-            queue.push(new searchContext(h, h));
-            pre1[h] = -1;
-            hos1[h] = h;
-        }
-
-
-        while (!queue.isEmpty()) {
-//            System.out.println("One step");
-            searchContext context = queue.removeLast();
-//            System.out.printf("%d %d\n", context.currentNode, context.hospital);
-            for (int adjacent : graph.adj(context.currentNode)) {
-                if (pre1[adjacent] == -2) {
-                    pre1[adjacent] = context.currentNode;
-                    hos1[adjacent] = context.hospital;
-                    queue.push(new searchContext(adjacent, context.hospital));
-                } else if (pre2[adjacent] == -2 && hos1[adjacent] != context.hospital) {
-                    pre2[adjacent] = context.currentNode;
-                    hos2[adjacent] = context.hospital;
-                    queue.push(new searchContext(adjacent, context.hospital));
-                }
-            }
-        }
-
-//        System.out.println(Arrays.toString(pre1));
-//        System.out.println(Arrays.toString(hos1));
-//        System.out.println(Arrays.toString(pre2));
-//        System.out.println(Arrays.toString(hos2));
-
-        for (int i = 0; i < graph.V(); i++) {
-            System.out.printf("Point %d:\n", i);
-
-            System.out.printf("First nearest hospital: %d\n", hos1[i]);
-            int next = pre1[i];
-            System.out.printf("Route: %d", i);
-            while (next != -1) {
-                System.out.printf(" -> %d", next);
-                next = hos1[next] == hos1[i] ? pre1[next] : pre2[next];
-            }
-            System.out.println();
-
-            System.out.printf("Second nearest hospital: %d\n", hos2[i]);
-            next = pre2[i];
-            System.out.printf("Route: %d", i);
-            while (next != -1) {
-                System.out.printf(" -> %d", next);
-                next = hos1[next] == hos2[i] ? pre1[next] : pre2[next];
-            }
-            System.out.println();
-            System.out.println();
-        }
-
-//        return null;
-    }
-
-    public class Answer {
-
+    public static Q4Solution.Answer solve(CityGraph graph) {
+        return Q4Solution.solve(graph, 2);
     }
 
     public static void main(String[] args) {
@@ -110,7 +29,8 @@ public class Q3Solution {
             builder.setHospital(scanner.nextInt());
         }
 
-        solve(builder.build());
+        Q4Solution.Answer answer = solve(builder.build());
+        answer.printout();
     }
 
 }
--- src/Q4Solution.java
+++ src/Q4Solution.java
@@ -8,10 +8,12 @@ public class Q4Solution {
 
         int currentNode;
         int hospital;
+        int distance;
 
-        searchContext(int c, int h) {
+        searchContext(int c, int h, int d) {
             currentNode = c;
             hospital = h;
+            distance = d;
         }
     }
 
@@ -23,19 +25,23 @@ public class Q4Solution {
      * @return
      */
 
-    public static void solve(CityGraph graph, int k) {
+    public static Answer solve(CityGraph graph, int k) {
         HashSet<Integer>[] hashSets = new HashSet[graph.V()];
         LinkedList<Integer>[] queues = new LinkedList[graph.V()];
+        LinkedList<Integer>[] distances = new LinkedList[graph.V()];
         LinkedList<searchContext> BFSQueue = new LinkedList<>();
 
 
         for (int i = 0; i < graph.V(); i++) {
             hashSets[i] = new HashSet<>();
             queues[i] = new LinkedList<>();
+            distances[i] = new LinkedList<>();
+
             if (graph.isHospital(i)) {
                 hashSets[i].add(i);
                 queues[i].push(i);
-                BFSQueue.push(new searchContext(i, i));
+                distances[i].push(0);
+                BFSQueue.push(new searchContext(i, i, 0));
             }
         }
 
@@ -45,22 +51,42 @@ public class Q4Solution {
                 if (hashSets[adj].size() < k && !hashSets[adj].contains(context.hospital)) {
                     hashSets[adj].add(context.hospital);
                     queues[adj].push(context.hospital);
-                    BFSQueue.push(new searchContext(adj, context.hospital));
+                    distances[adj].push(context.distance + 1);
+                    BFSQueue.push(new searchContext(adj, context.hospital, context.distance + 1));
                 }
             }
         }
 
-        for (int i = 0; i < graph.V(); i++) {
-            System.out.printf("Point %d:\n Nearest %d hospitals:", i, k);
-            while (!queues[i].isEmpty()) {
-                System.out.printf(" %d", queues[i].removeLast());
-            }
-            System.out.println();
-        }
+        return new Answer(queues, distances, graph, k);
     }
 
-    public class Answer {
+    public static class Answer {
+        private static LinkedList<Integer>[] queues, distances;
+        private static CityGraph graph;
+        private static int k;
 
+        public Answer(LinkedList<Integer>[] queues, LinkedList<Integer>[] distances,
+                      CityGraph graph, int k) {
+            Answer.queues = queues;
+            Answer.distances = distances;
+            Answer.graph = graph;
+            Answer.k = k;
+        }
+
+        public void printout() {
+            for (int i = 0; i < graph.V(); i++) {
+                System.out.printf("Point %d:\n", i);
+                if (queues[i].size() < k) {
+                    System.out.printf("  This point cannot reach %d hospitals.\n", k);
+                }
+                System.out.printf("  Nearest %d hospitals:\n", queues[i].size());
+                while (!queues[i].isEmpty()) {
+                    System.out.printf("    Hospital: %d ", queues[i].removeLast());
+                    System.out.printf("Distance: %d\n", distances[i].removeLast());
+                }
+                System.out.println();
+            }
+        }
     }
 
     public static void main(String[] args) {
@@ -83,6 +109,7 @@ public class Q4Solution {
 
         int k = scanner.nextInt();
 
-        solve(builder.build(), k);
+        Answer answer = solve(builder.build(), k);
+        answer.printout();
     }
 }
