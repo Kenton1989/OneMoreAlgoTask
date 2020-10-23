80fe43688537417d467b0e3013b915036441b52a
kenton <38125586+Kenton1989@users.noreply.github.com>
2020-10-23 17:50:08 +0800

    Template for Q1Q2 and brute force

--- src/Q1Q2Solution.java
+++ src/Q1Q2Solution.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.Queue;
 
 /**
@@ -10,25 +11,38 @@ public class Q1Q2Solution {
      * 
      * @param graph A undirected unweighted graph representing a city road network.
      * @return Two int array representing the answer.
+     * <p>
+     *      The first one is the distance between
      */
     public Answer solve(CityGraph graph) {
         int[] dist = new int[graph.V()];
+        int[] hosp = new int[graph.V()];
         int[] parent = new int[graph.V()];
         Arrays.fill(dist, -1);
+        Arrays.fill(hosp, -1);
         Arrays.fill(parent, -1);
 
         // Other Solution code
+        Queue<Integer> queue = new LinkedList<>();
 
-        return new Answer(dist, parent);
+        return new Answer(graph, dist, hosp, parent);
     }
 
     public class Answer {
+        public CityGraph graph;
         public int[] parent;
         public int[] dist;
+        public int[] hosp;
 
-        Answer(int[] nearest, int[] parent) {
-            this.dist = nearest;
+        Answer(CityGraph graph, int[] nearestDistance, int[] nearestHospital, int[] parent) {
+            this.graph = graph;
+            this.dist = nearestDistance;
+            this.hosp = nearestHospital;
             this.parent = parent;
+        }
+
+        public void printAnswer() {
+            
         }
     }
 }
