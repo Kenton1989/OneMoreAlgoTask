91301d17e9cf00558d721108ce5cd7c28bf6d99d
kenton <38125586+Kenton1989@users.noreply.github.com>
2020-10-29 12:04:43 +0800

    Doc Answer

--- src/Answer.java
+++ src/Answer.java
@@ -1,8 +1,15 @@
 import java.io.PrintStream;
 
 public abstract class Answer {
+    /**
+     * Print the answer with the given printer.
+     * @param printer the printer used to print answer.
+     */
     public abstract void printAns(PrintStream printer);
     
+    /**
+     * Print the answer to system standard output stream.
+     */
     public void printAns() {
         printAns(System.out);
     }
